18286a06c1043c3f3e1aafefbdb5058a1641eb72
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2022-08-16 14:12:43 -0300

    First stable version

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-# adjacent-element-selector-plugin
+# relationship-selector-plugin
 
 A plugin to select adjacent elements in Tailwind.
+
+## How to install
+
+```bash
+    npm install https://github.com/remy/nodemon.git
+```
+## User guide
+
+### Configure the tailwind.config.js
+
+```js
+    /** @type {import('tailwindcss').Config} */
+const plugin = require('relationship-selector-plugin');
+
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin({
+        nex: ['div', 'p'], // specific elements
+        bro: ['.some-class','text-sm'], // specific classes
+        son: ['li#id'], // compound selectors
+        dow: ['*'] // select all descendants
+    }
+    )],
+};
+```
+
+### Added prefixes
+
+ - nex:tailwind-class == tailwind-class + selectors-setted
+ - bro:tailwind-class == tailwind-class ~ selectors-setted
+ - son:tailwind-class == tailwind-class > selectors-setted
+ - dow:tailwind-class == tailwind-class selectors-setted
+
 
--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const plugin =  require('tailwindcss/plugin');
 
-const nextRules = [];
-const lestRules = [];
-const brotherRules = [];
-
-const conteiner =  function(){
+const conteiner =  function(rules){
     'use strict';
+    for(let i in rules)
+        conteiner[i] = rules[i];
     return plugin(
         function({addVariant}){
-            conteiner.nextRules.forEach((e) => addVariant('next',`& + ${e}`));
-            conteiner.lestRules.forEach((e) => addVariant('lest',`${e} + &`));
-            conteiner.brotherRules.forEach((e) => addVariant('brother',`& ~ ${e}`));
+            if(conteiner.nex instanceof Array)
+                conteiner.nex.forEach((e) => addVariant('nex',`& + ${e}`));
+            if(conteiner.bro instanceof Array)
+                conteiner.bro.forEach((e) => addVariant('bro',`& ~ ${e}`));
+            if(conteiner.son instanceof Array)
+                conteiner.son.forEach((e) => addVariant('son',`& > ${e}`));
+            if(conteiner.dow instanceof Array)
+                conteiner.dow.forEach((e) => addVariant('dow',`& ${e}`));
         }
     );
 };
 
-conteiner.nextRules = nextRules;
-conteiner.lestRules = lestRules;
-conteiner.brotherRules = brotherRules;
+conteiner.nex = [];
+conteiner.bro = [];//brother
+conteiner.son = [];
+conteiner.dow = [];// downward 
 
 module.exports = conteiner;
